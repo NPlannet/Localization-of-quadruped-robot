20156e9196966adfc88f6963eb5404c6167050e4
LucasVoelz <Lucas.voelz@googlemail.com>
2026-05-28 17:06:14 +0200

    included the navigation into the new exploration

--- robot_sim_sose/explore.py
+++ robot_sim_sose/explore.py
@@ -1,14 +1,19 @@
-from math import radians, atan2, cos, sin, isfinite
+from math import radians, isfinite, hypot
 import tf_transformations
-
+import numpy as np
 import rclpy
+from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import OccupancyGrid, Odometry
+import os
 
 from sensor_msgs.msg import LaserScan, Imu, Image
+from PIL import Image as PILImage
 
+from nav2_simple_commander.robot_navigator import BasicNavigator
+from geometry_msgs.msg import PoseStamped
 
 class RobotController(Node):
 
@@ -20,7 +25,9 @@ class RobotController(Node):
         self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
         self.create_subscription(Imu, '/imu', self.imu_callback, 10)
         self.create_subscription(Image, '/camera/image_raw', self.camera_callback, 10)
-
+        self.create_subscription(OccupancyGrid,'/map', self.map_callback, 10)
+        self.navigator = BasicNavigator()
+        
         self.current_yaw = 0.0
         self.current_x = 0.0
         self.current_y = 0.0
@@ -31,9 +38,10 @@ class RobotController(Node):
         self.right_distance = float('inf')
         self.imu_data = None
         self.camera_image = None
-        
+        self.map_data = None
         self.bridge = CvBridge()
-        self.explore_timer = self.create_timer(0.1, self.explore_step)  # this calls explore step every 0.1 seconds
+        self.explore_timer = self.create_timer(5.0, self.explore_step)
+        self.iteration = 0
         
     def odom_callback(self, msg):
         q = msg.pose.pose.orientation
@@ -47,16 +55,15 @@ class RobotController(Node):
         self.front_distance = get_range_at_angle(msg, 0)
         self.left_distance = get_range_at_angle(msg, 45)
         self.right_distance = get_range_at_angle(msg, -45)
-        self.get_logger().info(
-            f"LIDAR DISTANCES: front={self.front_distance:.2f}, "
-            f"left={self.left_distance:.2f}, right={self.right_distance:.2f}"
-        )
-        # self.get_logger().info(f"LIDAR received:\n{msg}")
-
+        # self.get_logger().info(
+        #     f"LIDAR DISTANCES: front={self.front_distance:.2f}, "
+        #     f"left={self.left_distance:.2f}, right={self.right_distance:.2f}"
+        # )
+        
         
     def imu_callback(self, msg):
         self.imu_data = msg
-        self.get_logger().info(f"IMU:\n{msg}")
+        
     
     def camera_callback(self, msg):
         try:
@@ -65,70 +72,112 @@ class RobotController(Node):
             self.get_logger().info(f"Camera image received: {cv_image.shape}")
         except Exception as e:
             self.get_logger().error(f"Camera conversion failed: {e}")
+            
+    def map_callback(self, msg):
+        self.map_info = msg.info
+
+        self.map_data = np.array(msg.data).reshape((msg.info.height, msg.info.width))
 
 
-    def stop(self):
-        msg = Twist()
-        self.cmd_pub.publish(msg)
-
-
-    def move_forward(self, distance, speed=0.5):
-        start_x = self.current_x
-        start_y = self.current_y
-    
-        msg = Twist()
-        msg.linear.x = speed
+    def save_map_image(self):
+        if self.map_data is not None:
+            os.makedirs("maps", exist_ok=True)
         
-        traveled = 0
-        while(traveled < distance):
-            traveled = ((self.current_x - start_x)**2 + (self.current_y - start_y)**2)**0.5
-            self.cmd_pub.publish(msg)
-            rclpy.spin_once(self, timeout_sec=0.01)
-        self.stop()
-
-    def rotate(self, degrees, angular_speed=0.5):
-        target = radians(degrees)
-        start = self.current_yaw
-    
-        msg = Twist()
-        if (target > 0):
-            msg.angular.z = angular_speed
-        else:
-            msg.angular.z = -angular_speed
-    
-        while (abs(atan2(sin((self.current_yaw - start)), cos((self.current_yaw - start)))) < abs(target)):
-            self.cmd_pub.publish(msg)
-            rclpy.spin_once(self, timeout_sec=0.01)
-    
-        self.stop()
-
-
+            img = np.zeros_like(self.map_data, dtype=np.uint8)
+        
+            img[self.map_data == -1] = 128
+            img[self.map_data == 0] = 255
+            img[self.map_data == 100] = 0
+        
+            image = PILImage.fromarray(img)
+            image.save(f"maps/map_{self.iteration:04d}.png")
+            self.iteration += 1
+            
     def explore_step(self):
-        msg = Twist()
+        if self.map_data is None:
+            return
+        self.save_map_image()
+        
+        if not self.navigator.isTaskComplete():
+            self.get_logger().info("Still navigating...")
+            return
+        
+        discover  = self.discover_next()
+        if discover is None:
+            self.get_logger().info("Nothing to discover found.")
+            return
+        gx, gy, _, = discover
+        wx, wy = self.grid_to_world(gx, gy)
+        self.get_logger().info(f"Navigating to grid=({gx}, {gy}) world=({wx:.2f}, {wy:.2f})")
+        self.navigate_to(wx, wy)
+        
 
-        if self.front_distance < 0.6:
-            msg.angular.z = 0.6 if self.left_distance > self.right_distance else -0.6
-        else:
-            msg.linear.x = 0.25
+        
+    def navigate_to(self, x, y):
+        goal = PoseStamped()
+        goal.header.frame_id = 'map'
+        goal.header.stamp = self.get_clock().now().to_msg()
+        goal.pose.position.x = x
+        goal.pose.position.y = y
+        goal.pose.orientation.w = 1.0
+        self.navigator.goToPose(goal)
 
-            if self.left_distance < 0.4:
-                msg.angular.z = -0.3
-            elif self.right_distance < 0.4:
-                msg.angular.z = 0.3
 
-        self.cmd_pub.publish(msg)
+    def grid_to_world(self, gx, gy):
+        resolution = self.map_info.resolution
+        wx = gx * resolution + self.map_info.origin.position.x
+        wy = gy * resolution + self.map_info.origin.position.y
+        return wx, wy
+    
+    
+    def discover_next(self, min_distance = 1.5):
+        if self.map_data is None:
+            return None
+    
+        frontiers = []
+        h, w = self.map_data.shape
+    
+        robot_gx = int((self.current_x - self.map_info.origin.position.x)/self.map_info.resolution)
+        robot_gy = int((self.current_y - self.map_info.origin.position.y)/ self.map_info.resolution)
+    
+        for y in range(1, h - 1):
+            for x in range(1, w - 1):
+    
+                if self.map_data[y, x] != 0:
+                    continue
+    
+                neighbors = [
+                    self.map_data[y + 1, x],
+                    self.map_data[y - 1, x],
+                    self.map_data[y, x + 1],
+                    self.map_data[y, x - 1],
+                ]
+    
+                if -1 in neighbors:
+                    distance = (hypot(x - robot_gx, y - robot_gy)* self.map_info.resolution)
+                    if distance < min_distance:
+                        continue
+                    frontiers.append((x, y, distance))
+    
+        if not frontiers:
+            return None
+    
+        frontiers.sort(key=lambda f: f[2])
+        return frontiers[0]
 
 
 def dynamic_explore():
     rclpy.init()
     robot = RobotController()
+    executor = MultiThreadedExecutor()
+    executor.add_node(robot)
 
     try:
-        rclpy.spin(robot)
+        executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
-        robot.stop()
+        executor.shutdown()
         robot.destroy_node()
         rclpy.shutdown()
     
